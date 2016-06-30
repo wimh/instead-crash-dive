8e399893e33a0fe36be4f6f58204639bc04ba8dc
wimh <wimh@users.noreply.github.com>
2016-06-30 11:47:26 +0200

    sorted rooms+objects

--- main_en.lua
+++ main_en.lua
@@ -1,7 +1,7 @@
 -- $Name:Crash Dive!$
 -- $Version:0.1.0$
 
--- vim: set fileencoding=utf-8 nobomb
+-- vim: set fileencoding=utf-8 nobomb foldmethod=syntax nofoldenable foldnestmax=1:
 
 instead_version '1.9.1'
 
@@ -19,14 +19,21 @@ game.act = 'Nothing happens'
 
 game.codepage="UTF-8"
 
-main = room {
-    nam = 'Crash Dive!',
-    obj = { 
-            vway('1', '{About original game}^^', 'about'),
-            vway('2', '{Start game}', 'intro')
-    },
-    hideinv = true,
-};
+
+function init()
+    take(eyes); 
+    take(ears); 
+    take(nose);
+    take(mouth); 
+    take(feet);
+    lifeon(enemy, 6);
+end; 
+
+global {
+    -- 0 = not holding breath
+    gl_holdbreathtimer = 0, 
+}
+
 
 about = room {
     nam = 'About original game',
@@ -44,9 +51,181 @@ about = room {
     hideinv = true,
 };
 
-global {
-    -- 0 = not holding breath
-    gl_holdbreathtimer = 0, 
+ballast_control = room {
+    nam = "Ballast control",
+    obj = {
+        'depth_gauge',
+        'red_button',
+    },
+    way = {
+        'command_station', -- E
+    }
+}
+
+command_station = room {
+    nam = 'Command station',
+    obj = {
+        'periscope',
+    },
+    way = {
+        vroom('Down', 'missile_control'),--command_station
+        'long_corridor', -- N
+        'ballast_control', -- W
+    },
+}
+
+closed_eyes = room {
+    nam = '',
+    dsc = [[You can't see anything!]],
+};
+
+crews_quarters = room {
+    nam = 'Crews quarters',
+    dsc = [[crews_quarters]],
+    way = {
+        vroom('Up', 'forward_passage'),
+        'torpedo_room', -- N
+        'missile_control', -- S
+        'shower_stalls', -- W
+    },
+}
+
+dead = room {
+    nam = 'You are dead',
+    hideinv = true,
+}
+
+depth_gauge = obj {
+    var {
+        depth = 0,
+    },
+    nam = 'Depth gauge',
+    dsc = 'There is a {depth gauge} at the wall',
+    act = function(s)
+        p(tostring(s.depth)..' fathoms') 
+    end,
+    life = function(s)
+        if s.depth < 128 then
+            s.depth = s.depth + 8
+            if s.depth == 128 then
+                lifeoff(s)
+                p(txtb('BANG!')..' sub hits bottom')
+            end
+        end
+    end,
+}
+
+ears = obj {
+    nam = '(My Ears)',
+    inv = [[You listen. Everything is silent as death.]],
+};
+
+enemy = obj {
+    var {
+        timer = 30,
+    },
+    nam = 'Enemy',
+    life = function(s)
+        if s.timer > 0 then
+            s.timer = s.timer - 1
+        end
+        -- p(tostring(s.timer))
+        -- p(tostring(depth_gauge.depth))
+        if s.timer == 0 and depth_gauge.depth == 0 then
+            if here() ~= dead then
+                walkin('dead')
+                p "The enemy captures the sub and kills you instantly!"
+            end
+        end
+    end,
+}
+
+equipment_bay = room {
+    nam = 'Equipment bay',
+    obj = {
+        'radiation_suit',
+    },
+    way = {
+        'missile_control', -- W
+    },
+}
+
+escape_tube = room {
+    forcedsc = true,
+    nam = 'Escape tube',
+    dsc = [[You are in the escape tube. There is a {hatch|hatch} in the floor. It has a {hatch_handle|handle} to open it.]],
+    obj = { 
+        xact('hatch', 'It is airtight'),
+        xact('hatch_handle', code[[
+            if not disabled(path('Down')) then
+                p "You close the hatch."
+                path('Down'):disable()
+            else
+                p "You open the hatch."
+                path('Down'):enable()
+                lifeon(poison, 5)
+            end
+        ]]),
+        'screwdriver',
+    },
+    way = {
+        vroom('Down', 'forward_passage'):disable(),
+    },
+};
+
+eyes = obj {
+    nam = '(My Eyes)',
+    inv = function(s)
+        if here() == closed_eyes then
+            walkout() 
+            p [[You opened your eyes. It's good to be able to see everything again.]]
+        else
+            walkin(closed_eyes) 
+            p [[You closed your eyes.]]
+        end
+    end,
+};
+
+feet = obj {
+    nam = '(My Feet)',
+    inv = 'You jump a few times. Nothing happens...',
+};
+
+forward_passage = room {
+    nam = 'Forward passage',
+    dsc = [[forward_passage]],
+    way = {
+        vroom('Up', 'escape_tube'),
+        vroom('Down', 'crews_quarters'),
+        'long_corridor', -- S
+    },
+}
+
+gas_mask = obj {
+    nam = 'Gas mask',
+    dsc = code[[
+        if here() == weapons_locker then
+            p 'There is a {Gas mask} in an open locker.'
+        else
+            p 'There is a {Gas mask} on the floor.'
+        end
+    ]],
+    tak = 'You wear the Gas mask',
+    inv = function (s)
+        drop(s)
+        p 'You drop the Gas mask'
+    end,
+}
+
+grate = obj {
+    nam = 'Grate',
+    dsc = 'there is a {grate} on the astern wall',
+    act = 'it is screwed in place',
+    used = function(s, w)
+        if w == screwdriver then
+            p "You can't unscrew it, the screwdriver is too tiny."
+        end
+    end,
 }
 
 intro = room {
@@ -78,28 +257,35 @@ intro = room {
     hideinv = true,
 };
 
-eyes = obj {
-    nam = '(My Eyes)',
-    inv = function(s)
-        if here() == closed_eyes then
-            walkout() 
-            p [[You opened your eyes. It's good to be able to see everything again.]]
-        else
-            walkin(closed_eyes) 
-            p [[You closed your eyes.]]
-        end
-    end,
+long_corridor = room {
+    nam = 'Long corridor',
+    obj = {
+    },
+    way = {
+        'forward_passage', -- N
+        'command_station', -- S
+    },
+}
+
+main = room {
+    nam = 'Crash Dive!',
+    obj = { 
+            vway('1', '{About original game}^^', 'about'),
+            vway('2', '{Start game}', 'intro')
+    },
+    hideinv = true,
 };
 
-closed_eyes = room {
-    nam = '',
-    dsc = [[You can't see anything!]],
-};
-
-ears = obj {
-    nam = '(My Ears)',
-    inv = [[You listen. Everything is silent as death.]],
-};
+missile_control = room {
+    nam = "Missile control",
+    obj = {
+    },
+    way = {
+        vroom('Up', 'command_station'),
+        'crews_quarters', -- N
+        'equipment_bay', -- E
+    },
+}
 
 mouth = obj {
     nam = '(My Mouth)',
@@ -137,6 +323,18 @@ nose = obj {
     end,
 };
 
+periscope = obj {
+    nam = 'Periscope',
+    dsc = 'there is a {periscope} coming out of the roof.',
+    act = function(s)
+        if depth_gauge.depth == 0 then
+            p(txtb('^The enemy is approaching!!!!!!!!!^'))
+        else
+            p 'You can only see water'
+        end
+    end,
+}
+
 poison = obj {
     nam = 'Poison',
     life = function(s)
@@ -149,10 +347,31 @@ poison = obj {
     end,
 }
 
-feet = obj {
-    nam = '(My Feet)',
-    inv = 'You jump a few times. Nothing happens...',
-};
+radiation_suit = obj {
+    nam = 'Radiation suit',
+    dsc = 'There is a {radiation suit}',
+    tak = 'You wear the radiation suit',
+    inv = function (s)
+        drop(s) -- todo different action ;)
+        p 'You drop the radiation suit'
+    end,
+}
+
+red_button = obj {
+    nam = 'Red button',
+    dsc = 'with a {Red button} next to it.',
+    act = code[[
+        if depth_gauge.depth == 128 then
+            p 'nothing happens'
+        elseif live(depth_gauge) then
+            lifeoff(depth_gauge)
+            p 'sub levels off'
+        else
+            lifeon(depth_gauge, 8)
+            p 'sub dives'
+        end
+    ]],
+}
 
 screwdriver = obj {
     nam = 'Tiny screwdriver',
@@ -161,63 +380,21 @@ screwdriver = obj {
     inv = 'seems ordiary',
 }
 
-function init()
-    take(eyes); 
-    take(ears); 
-    take(nose);
-    take(mouth); 
-    take(feet);
-    lifeon(enemy, 6);
-end; 
-
-dead = room {
-    nam = 'You are dead',
-    hideinv = true,
+shampoo = obj {
+    nam = 'Shampoo',
+    dsc = 'There is a bottle of {shampoo}',
+    tak = 'You take the bottle',
+    inv = 'Looks like normal shampoo',
 }
 
-escape_tube = room {
-    forcedsc = true,
-    nam = 'Escape tube',
-    dsc = [[You are in the escape tube. There is a {hatch|hatch} in the floor. It has a {hatch_handle|handle} to open it.]],
-    obj = { 
-        xact('hatch', 'It is airtight'),
-        xact('hatch_handle', code[[
-            if not disabled(path('Down')) then
-                p "You close the hatch."
-                path('Down'):disable()
-            else
-                p "You open the hatch."
-                path('Down'):enable()
-                lifeon(poison, 5)
-            end
-        ]]),
-        'screwdriver',
+shower_stalls = room {
+    nam = 'Shower stalls',
+    obj = {
+        'grate',
+        'shampoo',
     },
     way = {
-        vroom('Down', 'forward_passage'):disable(),
-    },
-};
-
-
-
-forward_passage = room {
-    nam = 'Forward passage',
-    dsc = [[forward_passage]],
-    way = {
-        vroom('Up', 'escape_tube'),
-        vroom('Down', 'crews_quarters'),
-        'long_corridor', -- S
-    },
-}
-
-crews_quarters = room {
-    nam = 'Crews quarters',
-    dsc = [[crews_quarters]],
-    way = {
-        vroom('Up', 'forward_passage'),
-        'torpedo_room', -- N
-        'missile_control', -- S
-        'shower_stalls', -- W
+        'crews_quarters', -- E
     },
 }
 
@@ -237,182 +414,5 @@ weapons_locker = room {
     way = {
         'torpedo_room', -- W
     },
-}
-
-gas_mask = obj {
-    nam = 'Gas mask',
-    dsc = code[[
-        if here() == weapons_locker then
-            p 'There is a {Gas mask} in an open locker.'
-        else
-            p 'There is a {Gas mask} on the floor.'
-        end
-    ]],
-    tak = 'You wear the Gas mask',
-    inv = function (s)
-        drop(s)
-        p 'You drop the Gas mask'
-    end,
-}
-
-shower_stalls = room {
-    nam = 'Shower stalls',
-    obj = {
-        'grate',
-        'shampoo',
-    },
-    way = {
-        'crews_quarters', -- E
-    },
-}
-
-grate = obj {
-    nam = 'Grate',
-    dsc = 'there is a {grate} on the astern wall',
-    act = 'it is screwed in place',
-    used = function(s, w)
-        if w == screwdriver then
-            p "You can't unscrew it, the screwdriver is too tiny."
-        end
-    end,
-}
-
-shampoo = obj {
-    nam = 'Shampoo',
-    dsc = 'There is a bottle of {shampoo}',
-    tak = 'You take the bottle',
-    inv = 'Looks like normal shampoo',
-}
-
-missile_control = room {
-    nam = "Missile control",
-    obj = {
-    },
-    way = {
-        vroom('Up', 'command_station'),
-        'crews_quarters', -- N
-        'equipment_bay', -- E
-    },
-}
-
-equipment_bay = room {
-    nam = 'Equipment bay',
-    obj = {
-        'radiation_suit',
-    },
-    way = {
-        'missile_control', -- W
-    },
-}
-
-radiation_suit = obj {
-    nam = 'Radiation suit',
-    dsc = 'There is a {radiation suit}',
-    tak = 'You wear the radiation suit',
-    inv = function (s)
-        drop(s) -- todo different action ;)
-        p 'You drop the radiation suit'
-    end,
-}
-
-long_corridor = room {
-    nam = 'Long corridor',
-    obj = {
-    },
-    way = {
-        'forward_passage', -- N
-        'command_station', -- S
-    },
-}
-
-command_station = room {
-    nam = 'Command station',
-    obj = {
-        'periscope',
-    },
-    way = {
-        vroom('Down', 'missile_control'),--command_station
-        'long_corridor', -- N
-        'ballast_control', -- W
-    },
-}
-
-periscope = obj {
-    nam = 'Periscope',
-    dsc = 'there is a {periscope} coming out of the roof.',
-    act = function(s)
-        if depth_gauge.depth == 0 then
-            p(txtb('^The enemy is approaching!!!!!!!!!^'))
-        else
-            p 'You can only see water'
-        end
-    end,
-}
-
-enemy = obj {
-    var {
-        timer = 30,
-    },
-    nam = 'Enemy',
-    life = function(s)
-        if s.timer > 0 then
-            s.timer = s.timer - 1
-        end
-        -- p(tostring(s.timer))
-        -- p(tostring(depth_gauge.depth))
-        if s.timer == 0 and depth_gauge.depth == 0 then
-            if here() ~= dead then
-                walkin('dead')
-                p "The enemy captures the sub and kills you instantly!"
-            end
-        end
-    end,
-}
-
-ballast_control = room {
-    nam = "Ballast control",
-    obj = {
-        'depth_gauge',
-        'red_button',
-    },
-    way = {
-        'command_station', -- E
-    }
-}
-
-depth_gauge = obj {
-    var {
-        depth = 0,
-    },
-    nam = 'Depth gauge',
-    dsc = 'There is a {depth gauge} at the wall',
-    act = function(s)
-        p(tostring(s.depth)..' fathoms') 
-    end,
-    life = function(s)
-        if s.depth < 128 then
-            s.depth = s.depth + 8
-            if s.depth == 128 then
-                lifeoff(s)
-                p(txtb('BANG!')..' sub hits bottom')
-            end
-        end
-    end,
-}
-
-red_button = obj {
-    nam = 'Red button',
-    dsc = 'with a {Red button} next to it.',
-    act = code[[
-        if depth_gauge.depth == 128 then
-            p 'nothing happens'
-        elseif live(depth_gauge) then
-            lifeoff(depth_gauge)
-            p 'sub levels off'
-        else
-            lifeon(depth_gauge, 8)
-            p 'sub dives'
-        end
-    ]],
 }
 
