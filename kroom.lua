3ebdc2f19962baf7cbb2e4df88f6a7a29ae3ca50
wimh <wimh@users.noreply.github.com>
2016-07-07 19:10:05 +0200

    add license notice

--- kroom.lua
+++ kroom.lua
@@ -1,3 +1,7 @@
+-- This file is licensed under the Apache License, Version 2.0
+-- <http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+-- <http://opensource.org/licenses/MIT>, at your option.
+
 require "kbd"
 
 stead.kroom = function(where, direction)
